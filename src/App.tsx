4b9cff62e503c8d7703aeecb37b939e3944d5586
ArturBaybulatov <ArturBaybulatov@gmail.com>
2021-05-18 23:58:17 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,138 @@
-import {message} from 'antd';
+import {notification} from 'antd';
 
-const getAdamantium = () => {};
-const getPlatinum = () => {};
-const getGold = () => {};
-const getSilver = () => {};
-const getBronze = () => {};
+const mayThrowFactory = (msg: string) => () => {
+  if (Math.random() < 0.5) {
+    throw new Error(msg);
+  }
+};
+
+const getAdamantium = mayThrowFactory('Adamantium was taken by someone else');
+const getPlatinum = mayThrowFactory('Platinum was taken by someone else');
+const getGold = mayThrowFactory('Gold was taken by someone else');
+const getSilver = mayThrowFactory('Silver was taken by someone else');
+const getBronze = mayThrowFactory('Bronze was taken by someone else');
 
 const example = () => {
   try {
     getGold();
-  } catch {
-    message.error('Failed to get gold');
+  } catch (err) {
+    notification.error({
+      message: 'Failed to get gold',
+      description: err.message,
+    });
 
     try {
       getSilver();
-    } catch {
-      message.error('Failed to get silver');
+    } catch (err) {
+      notification.error({
+        message: 'Failed to get silver',
+        description: err.message,
+      });
 
       try {
         getBronze();
-      } catch {
-        message.error('Failed to get any medal');
+      } catch (err) {
+        notification.error({
+          message: 'Failed to get any medal',
+          description: err.message,
+        });
+
         return {error: true, message: 'Failed to get any medal'};
       }
 
-      message.success('Bronze has been won');
+      notification.success({message: 'Got bronze'});
       return {error: false, medal: 'bronze'};
     }
 
-    message.success('Silver has been won');
+    notification.success({message: 'Got silver'});
     return {error: false, medal: 'silver'};
   }
 
-  message.success('Gold has been won');
+  notification.success({message: 'Got gold'});
 
   try {
     getPlatinum();
-  } catch {
-    message.error('Failed to get platinum');
+  } catch (err) {
+    notification.error({
+      message: 'Failed to get platinum',
+      description: err.message,
+    });
+
     return {error: false, medal: 'gold'};
   }
 
-  message.success('Platinum has been won');
+  notification.success({message: 'Got platinum'});
 
   try {
     getAdamantium();
-  } catch {
-    message.error('Failed to get adamantium');
+  } catch (err) {
+    notification.error({
+      message: 'Failed to get adamantium',
+      description: err.message,
+    });
+
     return {error: false, medal: 'platinum'};
   }
 
-  message.success('Adamantium has been won');
+  notification.success({message: 'Got adamantium'});
   return {error: false, medal: 'adamantium'};
 };
 
 const example2 = () => {
   try {
     getGold();
-    message.success('Gold has been won');
+    notification.success({message: 'Got gold'});
 
     try {
       getPlatinum();
-      message.success('Platinum has been won');
+      notification.success({message: 'Got platinum'});
 
       try {
         getAdamantium();
-        message.success('Adamantium has been won');
+        notification.success({message: 'Got adamantium'});
         return {error: false, medal: 'adamantium'};
-      } catch {
-        message.error('Failed to get adamantium');
+      } catch (err) {
+        notification.error({
+          message: 'Failed to get adamantium',
+          description: err.message,
+        });
+
         return {error: false, medal: 'platinum'};
       }
-    } catch {
-      message.error('Failed to get platinum');
+    } catch (err) {
+      notification.error({
+        message: 'Failed to get platinum',
+        description: err.message,
+      });
+
       return {error: false, medal: 'gold'};
     }
-  } catch {
-    message.error('Failed to get gold');
+  } catch (err) {
+    notification.error({
+      message: 'Failed to get gold',
+      description: err.message,
+    });
 
     try {
       getSilver();
-      message.success('Silver has been won');
+      notification.success({message: 'Got silver'});
       return {error: false, medal: 'silver'};
-    } catch {
-      message.error('Failed to get silver');
+    } catch (err) {
+      notification.error({
+        message: 'Failed to get silver',
+        description: err.message,
+      });
 
       try {
         getBronze();
-        message.success('Bronze has been won');
+        notification.success({message: 'Got bronze'});
         return {error: false, medal: 'bronze'};
-      } catch {
-        message.error('Failed to get any medal');
+      } catch (err) {
+        notification.error({
+          message: 'Failed to get any medal',
+          description: err.message,
+        });
+
         return {error: true, message: 'Failed to get any medal'};
       }
     }
